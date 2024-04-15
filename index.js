ff14a95428f411d51b351f7c37f2f8551e058ca9
Eligio93 <eligio.cristantielli@gmail.com>
2024-04-15 19:38:10 +1000

    fix error code

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ http.createServer(function (req, res) {
     fs.readFile(pagePath, function (err, data) {
       if (err) {
         fs.readFile('./404.html', function (err, data) {
-          res.writeHead(200, { 'Content-Type': 'text/html' });
+          res.writeHead(404, { 'Content-Type': 'text/html' });
           res.write(data);
           return res.end();
         });
