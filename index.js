2894251b9a20acd33857891118974f5d27c7317f
Eligio93 <eligio.cristantielli@gmail.com>
2024-04-15 19:29:15 +1000

    ends developing

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
+import { URL } from 'url';
 import http from 'http';
 import fs from 'fs'
 
 
 http.createServer(function (req, res) {
-  fs.readFile('index.html',function(err,data){
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
-  })
-  
+  let baseUrl = new URL(req.url, 'http://localhost:8080');
+  console.log(baseUrl.pathname)
+  if (baseUrl.pathname == '/') {
+    fs.readFile('index.html', function (err, data) {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.write(data);
+      return res.end();
+    })
+  } else {
+    let pagePath = '.' + baseUrl.pathname + '.html'
+    fs.readFile(pagePath, function (err, data) {
+      if (err) {
+        fs.readFile('./404.html', function (err, data) {
+          res.writeHead(200, { 'Content-Type': 'text/html' });
+          res.write(data);
+          return res.end();
+        });
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(data);
+        return res.end();
+      }
+
+    })
+  }
 }).listen(8080);
