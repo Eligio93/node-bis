dc2cb516caf134afb02c7b31e60d0803fd72d6ad
Eligio93 <eligio.cristantielli@gmail.com>
2024-04-19 09:21:31 +1000

    translate previous server into express

--- index.js
+++ index.js
@@ -1,30 +1,44 @@
 import { URL } from 'url';
 import http from 'http';
 import fs from 'fs'
+import express from 'express'
 
+const app = express();
 
-http.createServer(function (req, res) {
-  let baseUrl = new URL(req.url, 'http://localhost:8080');
-  if (baseUrl.pathname == '/') {
-    fs.readFile('index.html', function (err, data) {
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.write(data);
-      return res.end();
-    })
-  } else {
-    let pagePath = '.' + baseUrl.pathname + '.html'
-    fs.readFile(pagePath, function (err, data) {
-      if (err) {
-        fs.readFile('./404.html', function (err, data) {
-          res.writeHead(404, { 'Content-Type': 'text/html' });
-          res.write(data);
-          return res.end();
-        });
-      } else {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write(data);
-        return res.end();
-      }
-    })
-  }
-}).listen(8080);
+app.get('/', (req, res) => {
+    fs.readFile('index.html', (err, data) => {
+        if (err) {
+            res.status(500).send('Errore nel caricamento della pagina');
+        } else {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(data);
+            res.end();
+        }
+    });
+});
+
+app.use((req, res) => {
+    const pagePath = `.${req.url}.html`;
+    fs.readFile(pagePath, (err, data) => {
+        if (err) {
+            fs.readFile('./404.html', (err, data) => {
+                if (err) {
+                    res.status(404).send('Pagina non trovata');
+                } else {
+                    res.writeHead(404, { 'Content-Type': 'text/html' });
+                    res.write(data);
+                    res.end();
+                }
+            });
+        } else {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(data);
+            res.end();
+        }
+    });
+});
+
+const PORT = 8080;
+app.listen(PORT, () => {
+    console.log(`Server in ascolto sulla porta ${PORT}`);
+});
